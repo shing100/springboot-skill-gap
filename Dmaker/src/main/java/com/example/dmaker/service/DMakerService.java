b0c89df2c7c117a16ba5756308ab472f66dee2d1
limgeun <shing100@naver.com>
2023-05-11 19:10:20 +0900

    transaction

--- Dmaker/src/main/java/com/example/dmaker/service/DMakerService.java
+++ Dmaker/src/main/java/com/example/dmaker/service/DMakerService.java
@@ -9,23 +9,42 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class DMakerService {
     private final DeveloperRepository developerRepository;
+    private final EntityManager em;
 
+    // ACID
+    // Atomicity: 원자성
+    // Consistency: 일관성
+    // Isolation: 고립성
+    // Durability: 지속성
     @Transactional
     public void createDeveloper() {
-        Developer developer = Developer.builder()
-                .developerLevel(DeveloperLevel.JUNIOR)
-                .developerSkillType(DeveloperSkillType.FRONT_END)
-                .experienceYears(2)
-                .name("Olaf")
-                .age(5)
-                .build();
+        EntityTransaction transaction = em.getTransaction();
+        try {
+            transaction.begin();
 
-        developerRepository.save(developer);
+            Developer developer = Developer.builder()
+                    .developerLevel(DeveloperLevel.JUNIOR)
+                    .developerSkillType(DeveloperSkillType.FRONT_END)
+                    .experienceYears(2)
+                    .name("Olaf")
+                    .age(5)
+                    .build();
+
+            developerRepository.save(developer);
+        } catch (Exception e) {
+            log.error("error: {}", e);
+            transaction.rollback();
+        } finally {
+            transaction.commit();
+        }
     }
 
 
