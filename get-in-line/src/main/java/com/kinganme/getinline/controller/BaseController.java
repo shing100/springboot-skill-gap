8c1a2f18ddcfab9308c7b57daaff8931daed0e98
Lim Geun <shing100@naver.com>
2023-07-01 15:04:51 +0900

    controller

--- get-in-line/src/main/java/com/kinganme/getinline/controller/BaseController.java
+++ get-in-line/src/main/java/com/kinganme/getinline/controller/BaseController.java
@@ -1,4 +1,20 @@
 package com.kinganme.getinline.controller;
 
-public class BaseController {
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BaseController implements ErrorController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/error")
+    public String error() {
+        return "error";
+    }
 }
