fa5e383d84eb7a729aca2b40ecd9b96b63338372
Lim Geun <shing100@naver.com>
2023-07-03 22:13:36 +0900

    [20230703] gradle, gitignore 추가

---
+++ hangangview/src/main/java/comn/kingname/hangangview/HangangviewApplication.java
@@ -0,0 +1,28 @@
+package comn.kingname.hangangview;
+
+import comn.kingname.hangangview.domain.Account;
+import comn.kingname.hangangview.properties.TelegramProperties;
+import comn.kingname.hangangview.service.AccountService;
+import comn.kingname.hangangview.util.TelegramSender;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.List;
+
+@Slf4j
+@EnableScheduling
+@SpringBootApplication
+@RequiredArgsConstructor
+public class HangangviewApplication {
+
+    private final TelegramSender telegramSender;
+    private final AccountService accountService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(HangangviewApplication.class, args);
+    }
+}
---
+++ hangangview/src/test/java/comn/kingname/hangangview/HangangviewApplicationTests.java
@@ -0,0 +1,13 @@
+package comn.kingname.hangangview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangangviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
